12ced3bcd4b41fc1c3832428c8a710dffc2fce10
Artūras <arturaspoliksa@gmail.com>
2024-07-25 21:35:37 +0300

    Store edit
    Can't buy if insufficient funds

--- Assets/Scripts/Logic/TraderItemUI.cs
+++ Assets/Scripts/Logic/TraderItemUI.cs
@@ -8,6 +8,11 @@ public class TraderItemUI : MonoBehaviour
 {
     private Item _item;
     private GameObject _storeMenu;
+    private Button _button;
+    private TextMeshProUGUI _name;
+    private Image _image;
+    private TextMeshProUGUI _description;
+
 
     // Public property with getter and setter
     public Item item
@@ -32,6 +37,10 @@ public class TraderItemUI : MonoBehaviour
     void Start()
     {
         _storeMenu = GameObject.Find("StoreMenu");
+        _name = _storeMenu.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        _image = _storeMenu.transform.GetChild(1).GetChild(1).gameObject.GetComponent<Image>();
+        _description = _storeMenu.transform.GetChild(1).GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
+        _button = _storeMenu.transform.GetChild(1).GetChild(3).gameObject.GetComponent<Button>();
     }
 
     // Update is called once per frame
@@ -55,8 +64,10 @@ public class TraderItemUI : MonoBehaviour
     public void PickThisItem()
     {
         if(_item == null) return;
-        _storeMenu.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = _item.itemName;
-        _storeMenu.transform.GetChild(1).GetChild(1).gameObject.GetComponent<Image>().sprite = _item.icon;
-        _storeMenu.transform.GetChild(1).GetChild(2).gameObject.GetComponent<TextMeshProUGUI>().text = _item.description;
+        _name.text = _item.itemName;
+        _image.sprite = _item.icon;
+        _description.text = _item.description;
+        if(_item.value <= CurrencyManager.Instance.totalCurrency) _button.interactable = true;
+        else _button.interactable = false;
     }
 }
